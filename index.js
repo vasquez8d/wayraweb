2dd7d7dbf4afa6950f85aa9c2b380ae5eb1fa448
vasquez8d <vasquez8d@gmail.com>
2017-10-18 23:07:08 -0500

    Implementacion jade views

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
+app.set("view engine","jade");
+app.use(express.static("public"));
+
 app.get("/", function(request,response){
-   response.send("Hola_Alex")
+   response.render("index")
+});
+
+app.get("/contact", function(request,response){
+   response.render("contact")
+});
+
+app.get("/servicios", function(request,response){
+   response.render("servicios")
+});
+
+app.get("/galeria", function(request,response){
+   response.render("galeria")
 });
 
 var port = process.env.PORT || 1337;
