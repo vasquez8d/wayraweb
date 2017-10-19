d24061dcb819462227d55215b64edc6c5d08c050
Alexsandre Vasquez <vasquez8d@gmail.com>
2017-10-18 22:19:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello World Alex!");
 
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
