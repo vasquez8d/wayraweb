ccf183803122c13c75e7428b31b8ecf976e2017d
vasquez8d <vasquez8d@gmail.com>
2017-10-18 22:37:16 -0500

    Agregado express js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+app.get("/", function(request,response){
+   response.writeHead(200, {"Content-Type": "text/plain"});
+   response.render("index")
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(8080);
