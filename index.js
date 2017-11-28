439fda4955774499dea52548d54a92878177938c
vasquez8d <vasquez8d@gmail.com>
2017-11-27 20:41:22 -0500

    Trabajo vistas de servicios

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ app.get("/", function(request,response){
    response.render("index")
 });
 
-app.get("/contact", function(request,response){
+app.get("/contacto", function(request,response){
    response.render("contact")
 });
 
-app.get("/servicios", function(request,response){
-   response.render("servicios")
+app.get("/productos", function(request,response){
+   response.render("productos")
 });
 
 app.get("/galeria", function(request,response){
    response.render("galeria")
 });
 
+app.get("/control-aviar", function(request,response){
+   response.render("service1")
+});
+
+app.get("/exhibiciones", function(request,response){
+   response.render("service2")
+});
+
+app.get("/publicidad", function(request,response){
+   response.render("service3")
+});
 var port = process.env.PORT || 1337;
 app.listen(port);
 
